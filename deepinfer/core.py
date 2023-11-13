7463a9ff47378c1267c30a482bd820bb2dd63416
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2023-11-13 09:04:57 -0800

    refactored code for computing the abstract representation of a model

--- deepinfer/core.py
+++ deepinfer/core.py
@@ -2,8 +2,55 @@ import keras
 import numpy as np
 import pandas as pd
 
+from keras.src.engine.base_layer import Layer
+
+
 PREDICTION_INTERVALS = [0.95]
 CONDITIONS = ['>=', '<=', '>', '<', '==', '!=']
+
+
+def get_layer_representation(layer: Layer):
+    """
+    This function computes the inverse function (γ) of a layer’s weight matrix (W) where γ (W) ::= (Wt.W) −1
+    :param layer: a layer of a model
+    :return: tuple: inverse function, gamma, weight matrix, bias, activation function
+    """
+    weight_matrix = layer.get_weights()[0]
+    bias = layer.get_weights()[1]
+    activation_func = layer.get_config()['activation']
+    weight_matrix_tr = np.transpose(weight_matrix)
+    symmetric_matrix = np.matmul(weight_matrix, weight_matrix_tr)
+    symmetric_matrix_inverse = np.linalg.inv(symmetric_matrix)
+    gamma = np.matmul(weight_matrix_tr, symmetric_matrix_inverse)
+    gamma_transpose = np.transpose(gamma)
+
+    return gamma_transpose, gamma, weight_matrix, bias, activation_func
+
+
+def get_abstract_representation(model: keras.Model):
+    """
+    This function computes the abstract representation of a model.
+
+    :param model: A Keras model.
+    :return:
+        - weights (List[np.ndarray]): List of weight matrices for each layer.
+        - biases (List[np.ndarray]): List of bias vectors for each layer.
+        - gammas (List[np.ndarray]): List of gamma matrices for each layer.
+        - activation_functions (List[str]): List of activation functions for each layer.
+        - inverse_functions (List[np.ndarray]): List of inverse gamma matrices for each layer.
+    """
+    model_size = len(model.layers)
+
+    print(f"Number of layers in the model: {model_size}")
+
+    weights, biases, gammas, activation_functions, inverse_functions = zip(
+        *[
+            get_layer_representation(model.layers[i])
+            for i in range(model_size)
+        ]
+    )
+
+    return list(weights), list(biases), list(gammas), list(activation_functions), list(inverse_functions), model_size
 
 
 def log(weights: list, biases: list, gammas: list, activation_functions: list):
@@ -23,46 +70,15 @@ def log(weights: list, biases: list, gammas: list, activation_functions: list):
 
 
 def infer_data_precondition(model: keras.Model, dataset: pd.DataFrame) -> dict:
-    num_model_layers = len(model.layers)
     Q = []  # postconditions
 
-    print(f"Number of model's layers: {num_model_layers}")
-
-    # Getting Weights and Biases for each layer and Compute Function variable \gamma
-    weight_matrix = np.array([])
-    weights = []  # Storing weight matrices
-    biases = []  # Storing Biases
-    gammas = []  # inverse functions (γ) of layer’s weight matrix
-    activation_functions = []  # Activation function list
-    Gamma_tr = []  # inverse functions (γ) of layer’s weight matrix
-    X = []
-    # if 'dense' in layer.name or 'Dense' in str(model.layers[i]):
-    #    print("For dense based network")
-
-    for i in range(0, num_model_layers):
-        print(i)
-        weight_matrix = model.layers[i].get_weights()[0]
-        weights.append(weight_matrix)
-        b = model.layers[i].get_weights()[1]
-        biases.append(b)
-        a = model.layers[i].get_config()['activation']
-        activation_functions.append(a)
-        w_tr = np.transpose(weight_matrix)
-        # print(f'Array:\n{w}')
-        # print(f'Transposed Array:\n{w_tr}')
-        A = np.matmul(weight_matrix, w_tr)
-        A_inv = np.linalg.inv(A)
-        gamma = np.matmul(w_tr, A_inv)
-        gammas.append(gamma)
-        gamma_transpose = np.transpose(gamma)
-        Gamma_tr.append(gamma_transpose)
-
+    weights, biases, gammas, activation_functions, inverse_functions, model_size = get_abstract_representation(model)
     log(weights, biases, gammas, activation_functions)
 
-    for i in range(num_model_layers):
+    for i in range(model_size):
         for j in range(len(PREDICTION_INTERVALS)):
             for k in range(len(CONDITIONS)):
-                M = np.matmul((Gamma_tr[i] * PREDICTION_INTERVALS[j]), - (biases[i]))
+                M = np.matmul((inverse_functions[i] * PREDICTION_INTERVALS[j]), - (biases[i]))
                 print("X_", i + 1, "postcondition:", CONDITIONS[k], PREDICTION_INTERVALS[j])
                 print(M)
 
@@ -73,29 +89,29 @@ def infer_data_precondition(model: keras.Model, dataset: pd.DataFrame) -> dict:
         N0 = N[0]
         if l == 1:
             if N0 == 'linear':
-                M = np.matmul((Gamma_tr[i] * Q), - (biases[i]))
+                M = np.matmul((inverse_functions[i] * Q), - (biases[i]))
                 print("X_", i + 1, "postcondition:", CONDITIONS[0], Q)
                 print(M)
 
             if N0 == 'relu':
                 try:
-                    M1 = np.matmul((Gamma_tr[i] * Q), - (biases[i]))
+                    M1 = np.matmul((inverse_functions[i] * Q), - (biases[i]))
                 except ValueError:
                     pass
-                M2 = np.matmul(Gamma_tr[i], - (biases[i]))
+                M2 = np.matmul(inverse_functions[i], - (biases[i]))
                 print("X_", i + 1, "postcondition:", CONDITIONS[0], Q)
                 # print(M1)
                 print(M2)
                 M = M2
 
             if N0 == 'sigmoid':
-                M = np.matmul((Gamma_tr[i] * np.log(Q / (1 - Q))), - (biases[i]))
+                M = np.matmul((inverse_functions[i] * np.log(Q / (1 - Q))), - (biases[i]))
                 print("X_", i + 1, "postcondition:", CONDITIONS[0], Q)
                 print(M)
 
             if N0 == 'tanh':
                 n_tanh = abs((Q - 1) / (Q + 1))
-                M = np.matmul((Gamma_tr[i] * (0.5 * np.log(n_tanh))), - (biases[i]))
+                M = np.matmul((inverse_functions[i] * (0.5 * np.log(n_tanh))), - (biases[i]))
                 print("X_", i + 1, "postcondition:", CONDITIONS[0], Q)
                 print(M)
 
@@ -126,9 +142,9 @@ def infer_data_precondition(model: keras.Model, dataset: pd.DataFrame) -> dict:
     Q = PREDICTION_INTERVALS[0]
     print(Q)
     # l=2
-    i = num_model_layers - 1
+    i = model_size - 1
 
-    wp = beta(activation_functions, Q, num_model_layers, i)
+    wp = beta(activation_functions, Q, model_size, i)
 
     # TODO: check this if is correct, as it omits the last feature (original code does this too)
     features = dataset.columns.to_numpy()[:-1]
